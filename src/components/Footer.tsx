312c43152720eb2b496d7374e371c684d2154082
mez32 <amesmer32@gmail.com>
2023-01-04 19:53:11 -0500

    Removed errant commented out code

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,6 @@
 import React, { useEffect } from 'react'
 
 const Footer: React.FC = () => {
-	// useEffect(() => {
-	//   const year = new Date().getFullYear()
-	// }, [])
 	return (
 		<div className='mx-1'>
 			<div className='bg-neutral-800 w-11/12 md:w-4/5 lg:w-3/5 lg:max-w-6xl p-2 mb-4 mx-auto justify-center items-center text-center border-black rounded-xl overflow-hidden shadow-md'>
